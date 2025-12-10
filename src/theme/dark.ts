ba112d0cd7293d949eec45dfbee942d8971b9b91
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-12-09 22:44:47 -0300

    Criado arquivo com as cores dos temas Claro e Escuro

---
+++ src/theme/dark.ts
@@ -0,0 +1,6 @@
+export default {
+  bgColorOne: '#000000',
+  bgColorTwo: '#121212',
+  textColor: '#F2F2F2',
+  shadow: 'rgba(0, 0, 0, 0.5)',
+};
---
+++ src/theme/light.ts
@@ -0,0 +1,6 @@
+export default {
+  bgColorOne: '#FFFFFF',
+  bgColorTwo: '#F2F2F2',
+  textColor: '#121212',
+  shadow: 'rgba(0, 0, 0, 0.5)',
+};
